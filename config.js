beb8943b70c59f2a0501f15dcdaddeb13f0426b0
Deneth400 <denethhansaka05@gmail.com>
2024-11-12 20:09:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "zmRzRDzb#xi4lLyEZ1DAmnl3z58X16TvmmWkMn3ulRzmM-xgSxPM",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/900435c6d3157c98c3c88.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Ado Thota Puluwannam Mata Kochchara Puluwanda",
 };
