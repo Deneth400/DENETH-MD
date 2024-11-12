4b624ed31f81083af9b20e6c82160411106eaa7b
Deneth400 <denethhansaka05@gmail.com>
2024-11-12 23:37:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ https://wa.me/94761864425
 
 > ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴇɴᴇᴛʜ-xᴅ ᴛᴇᴄʜ`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://github.com/Deneth400/DENETH-MD-HARD/blob/main/Images/DENETH-MD%20Alive.jpg?raw=true` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://github.com/Deneth400/DENETH-MD-HARD/blob/main/Images/DENETH-MD.jpg?raw=true` }, caption: up })
 
 }
 })
