db64fab57b72eee45834dfd811fa3b5ac62fddeb
Deneth400 <denethhansaka05@gmail.com>
2024-11-15 00:19:27 +0530

    Update botjid.js

--- plugins/botjid.js
+++ plugins/botjid.js
@@ -1,6 +1,7 @@
 const config = require('../config')
 const { cmd, commands } = require('../command')
 const { fetchJson } = require('../lib/functions');
+const fs = require('fs');
 
 cmd({
     pattern: "jid",
