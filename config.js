c9b9ae19b46b96fb4ef97bfd4b5b74a097b987f9
Deneth400 <denethhansaka05@gmail.com>
2024-11-14 05:53:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "zmRzRDzb#xi4lLyEZ1DAmnl3z58X16TvmmWkMn3ulRzmM-xgSxPM",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
-MODE: process.env.MODE || "private",
+MONGODB: process.env.MONGODB || "mongodb://mongo:yndAufzWqfHPOJWqEhyerJubJkjcjuZC@autorack.proxy.rlwy.net:16382",
 };
